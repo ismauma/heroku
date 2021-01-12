d2a12531b871734b297b9bb99a1123ca83d7f82e
ismauma <48435965+ismauma@users.noreply.github.com>
2021-01-12 20:01:04 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import flask
+
+app = flask.Flask(__name__)
+
+uri = 'mongodb+srv://admin:admin@cluster0.wepwf.mongodb.net/prueba?retryWrites=true&w=majority'
+
+@app.route('/songs', methods=['GET'])
+def fun():
+    client = pymongo.MongoClient(uri)
+
+    db = client.get_default_database()
+
+    songs = db['songs']
+
+    return songs
